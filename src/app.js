9bd2521be7769c48f798ec3aa251deefbe4502f3
Killiann Hervagault <killiannhervagault@MacBook-Pro-de-Killiann.local>
2018-03-30 23:21:43 +0200

    pure angular to play music now, no morel ibs added

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp', ['ngRoute', 'angularSoundManager']);
+var myApp = angular.module('myApp', ['ngRoute']);
 
 
 myApp.config(['$routeProvider', function($routeProvider){
@@ -42,9 +42,39 @@ myApp.run(function(){
 //use during app runs
 });
 
-myApp.controller('appController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
+myApp.controller('appController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams, ngAudio){
     console.log("appController is working")
 
+    //Player stuff
+    $scope.play = true;
+        play= true;
+        
+        $scope.switchBack = function(id) {
+			$http.get('/api/music/count').then(function(response){
+				count = response.data;
+				if(id - 1 == 0) {
+					$window.location.href = '#!/music/read/' + count;
+				} else {
+				$window.location.href = '#!/music/read/' + (id - 1);
+				}
+			});
+		}
+
+		$scope.switchNext = function(id) {
+			$http.get('/api/music/count').then(function(response){
+				count = response.data;
+				if(id + 1 > count) {
+					$window.location.href = '#!/music/read/' + 1;
+				} else {
+				$window.location.href = '#!/music/read/' + (id + 1);
+				}
+			});
+		}
+	
+	$scope.playDetect = function() {
+		$scope.play = !$scope.play;
+		play = !play;
+	}
     
     $scope.getSongs = function(){
 
@@ -52,6 +82,8 @@ myApp.controller('appController', ['$scope', '$http', '$routeParams', function($
         $scope.songs = response.data;
         });
     }
+
+    //Data from api
 
     $scope.getSongById = function() {
         var id= $routeParams.id;
@@ -74,6 +106,19 @@ myApp.controller('appController', ['$scope', '$http', '$routeParams', function($
             shiffman.remove();
         });
     }
+
+    $scope.playSong = function() {
+        var id= $routeParams.id;
+        $http.get('/api/music/id=' + id).then(function(response){ //All client logic goes in then
+            $scope.song = response.data;
+            console.log(response.data.path);
+            var audio = new Audio(response.data.path);
+            console.log(audio.duration);
+            audio.play();
+        });
+    }
+
+
 
 }]);
 
